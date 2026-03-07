e712b4ad8a31a3cb1bf9512bdd5214b35b9d0b5b
Divya-Sharma-CSE <divyash0242@gmail.com>
2026-03-07 16:42:15 +0530

    deploy

--- frontend/src/components/sidebar.jsx
+++ frontend/src/components/sidebar.jsx
@@ -6,16 +6,16 @@ function Sidebar() {
     <div className="sidebar">
       {/* Each icon links to a page */}
       <Link to="/personal">
-        <img src="images/profileIcon.png" className="icon" alt="Home" />
+        <img src="public/profileIcon.png" className="icon" alt="Home" />
       </Link>
       <Link to="/personal">
-        <img src="images/peopleIcon.png" className="icon" alt="Personal" />
+        <img src="public/peopleIcon.png" className="icon" alt="Personal" />
       </Link>
       <Link to="/add">
-        <img src="images/addIcon.png" className="icon" alt="Add" />
+        <img src="public/addIcon.png" className="icon" alt="Add" />
       </Link>
       <Link to="/settings">
-        <img src="images/settingIcon.png" className="icon" alt="Settings" />
+        <img src="public/settingIcon.png" className="icon" alt="Settings" />
       </Link>
     </div>
   );
